b744f4f98c1422cc128ac94c6c63e7c75a6b97f3
anthonymxk <anthonyxkeller@gmail.com>
2022-04-20 18:32:25 -0500

    Change website to flashcard maker instead Changed website from flashcard maker instead of a movie list because that is much more practical as a project and for using. Added a simple form for the user to submit information which will then be displayed in a new notecard.

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
-let library = []
+let library = [
+    {title: 'Wolf of Wallstreet', front: 'text'},
+    {title: 'Interstellar', front: 'textt'},
+]
 
-
-
-function book(name) {
-    this.name = name
+function notecard(title, front) {
+    this.title = title
+    this.front = front
 }
 
-function addBookToLibrary() {
-    let name = prompt('Name of book?')
-    let newBook = new book(name)
-    library.push(newBook)
-}
+//add new movie button
+let addNewButton = document.querySelector('#addNewNotecard')
 
-//add new movie
-let addNewButton = document.querySelector('#addNewMovie')
+//create HTML grid box to display the movie
+function addNotecardToHtmlLibrary(newNotecard) {
+    let notecardGrid = document.createElement('div')
+    let title = document.createElement('p')
+    let front = document.createElement('p')
+    let libraryDiv = document.querySelector('#library')
+    notecardGrid.className = 'notecardGrid'
+    title.textContent = `${newNotecard.title}`
+    front.textContent = `${newNotecard.front}`
+    notecardGrid.appendChild(title)
+    notecardGrid.appendChild(front)
+    libraryDiv.appendChild(notecardGrid)
+}
 
 //when button is clicked, ask for details,
 // create the object, then create a new div at the end of the grid
 //which displays the object
-addNewButton.addEventListener('click',function() {
-    let movieGridBox = document.createElement('div')
-    let movieTitle = document.createElement('p')
-    let libraryDiv = document.querySelector('#library')
-    movieGridBox.className = 'movieGridBox'
-    movieTitle.textContent = 'Example Movie'
-    movieGridBox.style.border = 'solid black'
-    movieGridBox.appendChild(movieTitle)
-    libraryDiv.appendChild(movieGridBox)
+addNewButton.addEventListener('click', function() {
+        //get user input
+        let cardTitle = document.querySelector('#titleForm')
+        let cardFront = document.querySelector('#frontForm')
+        // assign user input to object, add move object to JS array
+        library.push(new notecard(cardTitle.value,cardFront.value))
+        //add movie object to HTML library
+        addNotecardToHtmlLibrary(library[((library.length)-1)])
 })
+
+//create library of existing movies in library
+for (let i=0;i<library.length;i++) {
+    addNotecardToHtmlLibrary(library[i])
+}
+
+//get info from new notecard form
+
